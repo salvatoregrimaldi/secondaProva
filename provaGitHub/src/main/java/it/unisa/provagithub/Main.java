344b660efdea53f15e486ce61016f76433cdb0c2
salvatoregrima <grimaldisalvatore5@gmail.com>
2021-11-14 10:08:16 +0100

    very last hopeful

--- provaGitHub/src/main/java/it/unisa/provagithub/Main.java
+++ provaGitHub/src/main/java/it/unisa/provagithub/Main.java
@@ -11,6 +11,9 @@ package it.unisa.provagithub;
 public class Main {
     
     public static void main(String args[]){
-        System.out.println("Voglio ancora credere di aver fatto una buona performance ieri");
+        System.out.println("Ciao mondo");
+        System.out.println("Questa volta ho buone sensazioni");
+        System.out.println("Ho ancora buone sensazioni, nonostante tutto");
+        System.out.println("Dai ci credo ancora perché voglio essere ottimista!");   
     }
 }
